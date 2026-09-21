20b63678d0d8fa896a98c4dc7993ab03e9e66543
ibra-production <bahibarhouma56@gmail.com>
2026-09-21 09:06:02 +0100

    Fix portfolio image upload context export

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -2204,6 +2204,7 @@ export const AppProvider: React.FC<{
 
         addPortfolioItem,
         updatePortfolioItem,
+        uploadPortfolioImages,
         deletePortfolioItem,
 
         addVideoItem,
