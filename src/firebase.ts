a752ae45c9479ae13ca1865f93c9ba9194b40359
ibra-production <bahibarhouma56@gmail.com>
2026-09-24 06:40:32 +0100

    Export Firebase Storage for direct image uploads

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyATSuJTP2UVZoYcfaGILaY1e0D7qbEtdMc",
@@ -16,5 +17,6 @@ export const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
 
 export const vapidKey = "BOZm_0LT3xDvXw3yKFAmzlIbqhDzpJ8IMbc-b13DsPBhOf0YEZxr4at5noBWb_O-igMGxSVixuZIyPrVCJmlUuQ";
